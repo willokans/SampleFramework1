11bd8536d144f784a645b941ec9f19583bb0cc40
Will <willokans@gmail.com>
2017-09-25 01:24:09 +0100

    for mether

---
+++ src/main/java/methods.java
@@ -0,0 +1,30 @@
+import com.sun.javafx.tools.packager.Log;
+
+/**
+ * Created by Will on 25/09/2017.
+ */
+public class methods {
+
+
+//    //Verify that  'Campaigns' section displays all column
+//    String linkNames[] = {"Campaign", "Status", "Target", capacityOrQuota, "Used"};
+//    String [] linkNamesDescription = {"Campaign column is displayed", "Status column is displayed", "Target column is displayed", "Capacity column is displayed", "Used column is displayed"};
+//    String [] linkNameContain = {"Campaign", "Status", "Target", capacityOrQuota, "Used"};
+//
+//    String clickableLink [] = new String [linkNames.length];
+//
+//    String links = "";
+//
+//        for (int j =0; j < clickableLink.length; j++){
+//
+//        for (int k = 0; k < linkNames.length; k++){
+//
+//            links = ElevateCommon.getLocatorForLink.withName(linkNames[k]);
+//            Log.info("Checking if "+linkNames[k]+" is present");
+//            BrowserAction.assertTrue(BrowserAction.locator.getText(links).contains(linkNameContain[k]), Log.info(linkNamesDescription[k]));
+//            Log.info(linkNamesDescription[k]+" - SUCCESS");
+//
+//            clickableLink[j] = links;
+//        }
+//    }
+}
